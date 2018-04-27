da8fe432c223f75379c0f61b243c3391d0fd7d0c
Tai Linh Du <linh9a0910@gmail.com>
2018-04-27 16:49:18 +0200

    full version
    https://www.youtube.com/watch?v=Et2khGnrIqc&t=4306s

---
+++ FormUI/Dashboard.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUI
+{
+    public partial class Dashboard : Form
+    {
+        private List<Person> people = new List<Person>();
+        public Dashboard()
+        {
+            InitializeComponent();
+            //people connet to Listbox to display informations
+            UpdateBinding();
+        }
+
+        private void Form1_Load (object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateBinding()
+        {
+            peopleFoundListbox.DataSource = people;
+            peopleFoundListbox.DisplayMember = "FullInfo";
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            //Data Access
+            DataAccess db = new DataAccess();
+
+            people = db.GetPeople(lastNameText.Text);
+            UpdateBinding();
+        }
+
+        private void insertRecordButton_Click(object sender, EventArgs e)
+        {
+            DataAccess db = new DataAccess();
+
+            db.InsertPerson(firstNameInsText.Text, lastNameInsText.Text, emailAddressInsText.Text,
+                phoneNumberInsText.Text);
+
+            firstNameInsText.Text = "";
+            lastNameInsText.Text = "";
+            emailAdressInsText.Text = "";
+            phoneNumberInsText.Text = "";
+        }
+    }
+}
